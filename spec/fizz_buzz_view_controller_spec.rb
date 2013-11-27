a31d6bdffbf08f450118cf5e25a95c8bc1c0541d
Raymond T. Hightower <rth@wisdomgroup.com>
2013-11-26 20:57:33 -0600

    add mac bacon tests from fizzbuzzrm

---
+++ spec/fizz_buzz_view_controller_spec.rb
@@ -0,0 +1,35 @@
+describe "FizzBuzz calculator Function" do
+
+  it 'should return the fizz for 3' do
+    FizzBuzzViewController.fbcalc(3).should == 'fizz'
+  end
+
+  it 'should return the buzz for 5' do
+    FizzBuzzViewController.fbcalc(5).should == 'buzz'
+  end
+
+  (1..10).each do |this_number|
+    if this_number % 15 == 0
+      it 'should return buzz for multiples of 15' do
+         FizzBuzzViewController.fbcalc(this_number).should == 'fizzbuzz'
+      end
+    elsif this_number % 3 == 0
+      it 'should return fizz for multiples of 3' do
+         FizzBuzzViewController.fbcalc(this_number).should == 'fizz'
+      end
+    elsif this_number % 5 == 0
+      it 'should return buzz for multiples of 5' do
+         FizzBuzzViewController.fbcalc(this_number).should == 'buzz'
+      end
+    else
+      it 'should return the original number' do
+        FizzBuzzViewController.fbcalc(this_number).should == this_number
+      end
+   end
+  end
+
+  it 'should return the integer if it is not fizzable or buzzable' do
+    FizzBuzzViewController.fbcalc(38).should == 38
+  end
+
+end
